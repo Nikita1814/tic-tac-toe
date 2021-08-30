fa9f94a91d35281488df63ae03d29effb07dac89
Nikita1814 <kravchenko1814@gmail.com>
2021-08-30 22:48:11 +0300

    add function for task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,83 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.field = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    this.currentPlayer = "x";
+    this.winner = null;
+    this.turns = 0;
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.field[rowIndex][columnIndex] === null) {
+      this.field[rowIndex][columnIndex] = this.currentPlayer;
+      if (this.currentPlayer === "x") {
+        this.currentPlayer = "o";
+      } else {
+        this.currentPlayer = "x";
+      }
+
+      this.turns += 1;
+    }
+  }
+
+  isFinished() {
+    if (this.getWinner() !== null || this.isDraw()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getWinner() {
+    let f = this.field;
+    for (let i = 0; i < this.field.length; i++) {
+      if (f[i][0] !== null && f[i][0] === f[i][1] && f[i][0] === f[i][2]) {
+        this.winner = f[i][0];
+        return this.winner;
+      }
+      if (f[0][i] !== null && f[0][i] === f[1][i] && f[0][i] === f[2][i]) {
+        this.winner = f[0][i];
+        return this.winner;
+      }
     }
 
-    getCurrentPlayerSymbol() {
-
+    if (f[0][0] !== null && f[0][0] === f[1][1] && f[0][0] === f[2][2]) {
+      this.winner = f[0][0];
+      return this.winner;
     }
-
-    nextTurn(rowIndex, columnIndex) {
-
+    if (f[0][2] !== null && f[0][2] === f[1][1] && f[0][2] === f[2][0]) {
+      this.winner = f[0][2];
+      return this.winner;
     }
+    return this.winner;
+  }
 
-    isFinished() {
-
+  noMoreTurns() {
+    if (this.turns === 9) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    getWinner() {
-
+  isDraw() {
+    if (this.turns === 9 && this.getWinner() === null) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
